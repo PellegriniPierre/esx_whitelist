5acb62fd4bdc983eae29b688189fcd7e3c6af274
Pierre Pellegrini <pellegrini.pierre@gmail.com>
2021-03-08 08:06:17 +0100

    Maj langue fr

--- esx_whitelist.sql
+++ esx_whitelist.sql
@@ -1,7 +1,11 @@
 USE `es_extended`;
 
-CREATE TABLE `whitelist` (
-	`identifier` varchar(40) NOT NULL,
-
-	PRIMARY KEY (`identifier`)
+DROP TABLE IF EXISTS `whitelist`;
+CREATE TABLE IF NOT EXISTS `whitelist` (
+  `identifier` varchar(70) NOT NULL,
+  `last_connection` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `ban_reason` text DEFAULT NULL,
+  `ban_until` timestamp NULL DEFAULT NULL,
+  `vip` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`identifier`)
 );
